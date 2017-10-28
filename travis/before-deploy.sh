26f14da5122d351f8b034c7b402b38f46ca95938
cezar_1 <1280052+cbartosiak@users.noreply.github.com>
2017-10-28 18:19:10 +0200

    fixed scripts

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-if [ "$TRAVIS_BRANCH" =~ ^v[0-9]\.[0-9]\.[0-9]$ ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [ "$TRAVIS_BRANCH" =~ "^v[0-9]\.[0-9]\.[0-9]$" ] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     openssl aes-256-cbc -K $encrypted_c6e845ad902f_key -iv $encrypted_c6e845ad902f_iv -in travis/signing-key.asc.enc -out travis/signing-key.asc -d
     gpg --fast-import travis/signing-key.asc
 fi
