588596a89f6bf8e49f7e8fb822dec0c62bc8d3bb
cbartosiak <1280052+cbartosiak@users.noreply.github.com>
2017-10-29 18:02:40 +0100

    increment versions, fixed scripts

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-if [[ $TRAVIS_BRANCH =~ ^v[0-9]\.[0-9]\.[0-9]$ ]] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
+if [[ $TRAVIS_BRANCH =~ ^(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)\.(0|[1-9][0-9]*)$ ]] && [ "$TRAVIS_PULL_REQUEST" = "false" ]; then
     mvn deploy -P build-extras,sign --settings travis/settings.xml
 fi
